8a4890f9c849e85170acc527dec68e5526bd9b25
tanjimN4 <tanjimahemed7@gmail.com>
2024-09-07 01:16:20 +0600

    filnal

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const jwt=require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const cors = require('cors')
 
 
@@ -40,39 +40,39 @@ async function run() {
         const howCollection = client.db("Project-12").collection("doantionsData");
 
         //middlewares
-        const verifyToken=(req,res,next)=>{
+        const verifyToken = (req, res, next) => {
             console.log(req.headers.authorization);
-            
-            if(!req.headers.authorization){
-                return res.status(401).send({message:'forbidden access'})
+
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'forbidden access' })
             }
-            const token=req.headers.authorization.split(' ')[1]
-            jwt.verify(token,process.env.ACCESS_TOkEN,(err,decoded)=>{
-                if(err){
-                    return res.status(401).send({message:'unauthorized access'})
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token, process.env.ACCESS_TOkEN, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
                 }
-                req.decoded=decoded
+                req.decoded = decoded
                 next()
             })
-            
-            
+
+
         }
         //veriyfy admin
-        const verifyAdmin=async(req,res,next)=>{
-            const email=req.decoded.email
-            const query={email:email}
-            const user=await usersCollection.findOne(query)
-            const isAdmin=user?.role==='admin'
-            if(!isAdmin){
-                return res.status(403).send({message:'forbidden access'})
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            const isAdmin = user?.role === 'admin'
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' })
             }
             next()
         }
         //jwt
-        app.post('/jwt',async(req,res)=>{
-            const user= req.body
-            const token=jwt.sign(user,process.env.ACCESS_TOkEN,{expiresIn:'1h'})
-            res.send({token})
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOkEN, { expiresIn: '1h' })
+            res.send({ token })
         })
 
         app.get('/pets', async (req, res) => {
@@ -86,10 +86,72 @@ async function run() {
             const result = await petsCollection.insertOne(item)
             res.send(result)
         })
+        //delete a pet
+        app.delete('/petDelete/:id', verifyToken, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await petsCollection.deleteOne(query)
+            res.send(result)
+        }),
+            //update adoption data 
+            app.patch('/petAdopted/:id', async (req, res) => {
+                const id = req.params.id;
+                const filter = { _id: new ObjectId(id) };
+                const updateDoc = {
+                    $set: {
+                        adopted: true
+                    }
+                };
+                const result = await petsCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            });
+        //update my peats
+        app.patch('/pets/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedate = req.body
+            const query = { _id: new ObjectId(id) };
+            const update = {
+                $set: updatedate
+            }
+            const result = await petsCollection.updateOne(query, update);
+            res.send(result);
+        });
+
         app.get('/donation', async (req, res) => {
             const result = await donationCollection.find().toArray()
             res.send(result)
 
+        })
+        app.delete('/donation/:id', verifyToken, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await donationCollection.deleteOne(query)
+            res.send(result)
+        }),
+        app.patch('/donation/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedate = req.body
+            const query = { _id: new ObjectId(id) };
+            const update = {
+                $set: updatedate
+            }
+            const result = await donationCollection.updateOne(query, update);
+            res.send(result);
+        });
+        app.patch('/donation/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedate = req.body
+            const query = { _id: new ObjectId(id) };
+            const update = {
+                $set: updatedate
+            }
+            const result = await donationCollection.updateOne(query, update);
+            res.send(result);
+        });
+        app.post('/donation', async (req, res) => {
+            const item = req.body
+            const result = await donationCollection.insertOne(item)
+            res.send(result)
         })
         app.get('/donation/:id', async (req, res) => {
             const id = req.params.id;
@@ -102,23 +164,53 @@ async function run() {
             const query = { _id: new ObjectId(id) }
             const result = await petsCollection.findOne(query);
             res.send(result);
-        })
+        }),
+            //delet pet admin
+            app.delete('/delete/pets/:id', async (req, res) => {
+                const id = req.params.id
+                const query = { _id: new ObjectId(id) }
+                const result = await petsCollection.deleteOne(query)
+                res.send(result)
+            })
         app.post('/adopt/pet', async (req, res) => {
             const item = req.body
             const result = await adoptionCollection.insertOne(item)
             res.send(result)
         })
+        //get adopt data
+        app.get('/getAdoptPet', async (req, res) => {
+            const result = await adoptionCollection.find().toArray()
+            res.send(result)
+        })
+        //delete a data pet
+        app.delete('/delete/adopt/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await adoptionCollection.deleteOne(query)
+            res.send(result)
+        })
+        app.patch('/adopted/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    adopted: true
+                }
+            };
+            const result = await adoptionCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
         //update donation amount
         app.post('/update/donation/:id', async (req, res) => {
             const id = req.params.id
-            const newDonation=req.body.amount
+            const newDonation = req.body.amount
             const query = { _id: new ObjectId(id) }
             const update = {
                 $inc: {
-                    donatedAmount :newDonation
+                    donatedAmount: newDonation
                 }
             }
-            const result = await donationCollection.updateOne(query,update)
+            const result = await donationCollection.updateOne(query, update)
             res.send(result)
         })
         //how donated
@@ -127,48 +219,59 @@ async function run() {
             const result = await howCollection.insertOne(item)
             res.send(result)
         })
+        app.delete('/donater/refund/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await howCollection.deleteOne(query)
+            res.send(result)
+        }),
+        //get donater info
+        app.get('/donater/data', async (req, res) => {
+            const result = await howCollection.find().toArray()
+            res.send(result)
+        })
         //check admin
-        app.get('/users/admin/:email',verifyToken,async(req,res)=>{
-            const email=req.params.email
-            if(email !==req.decoded.email){
-                return res.status(403).send({message:'forbiden access'})
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbiden access' })
             }
 
-            const query ={email:email}
-            const user=await usersCollection.findOne(query)
-            let admin=false
-            if(user){
-                admin=user?.role==='admin'
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            let admin = false
+            if (user) {
+                admin = user?.role === 'admin'
             }
-            res.send({admin})
+            res.send({ admin })
         })
         //get user
-        app.get('/users',verifyToken,verifyAdmin,async(req,res)=>{
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
 
-            const result =await usersCollection.find().toArray()
+            const result = await usersCollection.find().toArray()
             res.send(result)
         })
         //create user
-        app.post('/users',async(req,res)=>{
-            const user=req.body
-            const query={email:user.email}
-            const existingUser=await usersCollection.findOne(query)
-            if(existingUser){
-                return res.send({message:"user Already exists",insertedID:null})
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query)
+            if (existingUser) {
+                return res.send({ message: "user Already exists", insertedID: null })
             }
-            const result=await usersCollection.insertOne(user)
+            const result = await usersCollection.insertOne(user)
             res.send(result)
         })
         //admin create
-        app.patch('/users/admin/:id',verifyToken,verifyAdmin,async(req,res)=>{
-            const id=req.params.id
-            const query={_id: new ObjectId(id)}
-            const role={
-                $set:{
-                    role:'admin'
+        app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const role = {
+                $set: {
+                    role: 'admin'
                 }
             }
-            const result=await usersCollection.updateOne(query,role)
+            const result = await usersCollection.updateOne(query, role)
             res.send(result)
         })
         app.post('/create-payment-intent', async (req, res) => {
